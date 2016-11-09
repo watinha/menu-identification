3fb22fd76da5cbba56c0b426b270a2d7dcb599b3
Willian Massami Watanabe <wwatanabe@utfpr.edu.br>
2016-11-09 16:53:38 -0200

    now extracting a few features for related work

--- window-controller.js
+++ window-controller.js
@@ -8,7 +8,99 @@
         function _meta_data(element) {
             var result = _position(element);
             result.html = element.outerHTML;
+            /* Related work */
+            result.presenceTable = _presence_of_element(element, 'table');
+            result.presenceUl = _presence_of_element(element, 'ul');
+            result.presenceInput = _presence_of_element(element, 'input');
+            result.presenceWidgetName = _menu_name(element);
+            result.presenceDate = _date(element);
+            result.presenceImg = _presence_of_element(element, 'img');
+            result.numberTextNodes = _number_of_textnodes(element);
+            result.proportionNumberTextNodes = _proportion_number_in_textnodes(element);
+            result.percentLinks = _80_percent_links(element);
             return result;
+        }
+        function _80_percent_links (element) {
+            var target = element,
+                table = target.querySelector("table"),
+                ul = target.querySelector("ul"),
+                childs, childs2;
+
+            if (table) {
+                childs = table.querySelectorAll("*").length;
+                childs2 = table.querySelectorAll("a").length;
+                if ((childs2/childs) > 0.8)
+                    return 1;
+                else
+                    return 0;
+            }
+            if (ul) {
+                childs = ul.querySelectorAll("*").length;
+                childs2 = ul.querySelectorAll("a").length;
+                if ((childs2/childs) > 0.8)
+                    return 1;
+                else
+                    return 0;
+            }
+            return 0;
+        }
+        function _proportion_number_in_textnodes (element) {
+            var target = element,
+                aux = null, elements = [],
+                number_of_numbers = 0;
+
+            elements.push(target);
+            while (elements.length != 0) {
+                aux = elements.pop();
+                for (var i = 0; i < aux.childNodes.length; i++) {
+                    if (aux.childNodes[i].nodeType === 3) {
+                        if (!isNaN(parseInt(aux.childNodes[i].nodeValue)))
+                            number_of_numbers++;
+                    }
+                    if (aux.childNodes[i].nodeType === 1)
+                        elements.push(aux.childNodes[i]);
+
+                }
+            }
+            return number_of_numbers;
+        }
+        function _number_of_textnodes (element) {
+            var target = element,
+                aux = null, elements = [],
+                number_of_child_nodes = 0;
+
+            elements.push(target);
+            while (elements.length != 0) {
+                aux = elements.pop();
+                for (var i = 0; i < aux.childNodes.length; i++) {
+                    if (aux.childNodes[i].nodeType === 3)
+                        number_of_child_nodes++;
+                    if (aux.childNodes[i].nodeType === 1)
+                        elements.push(aux.childNodes[i]);
+                }
+            }
+            return number_of_child_nodes;
+        }
+        function _date (element) {
+            var aux = (element.type) ? element.type : '',
+                childs = element.querySelectorAll("*");
+            for (var i = 0; i < childs.length; i++) {
+                aux += ' ' + childs[i].type;
+            };
+            return (aux.search('date') >= 0);
+        }
+        function _menu_name (element) {
+            var aux = (element.className) ? element.className : '',
+                childs = element.querySelectorAll("*");
+            for (var i = 0; i < childs.length; i++) {
+                aux += ' ' + childs[i].className;
+            };
+            return (aux.search('menu') >= 0 ||
+                    aux.search('dropdown') >= 0 ||
+                    aux.search('drop-down') >= 0);
+        }
+        function _presence_of_element (element, tagName) {
+            return (element.querySelectorAll(tagName).length !== 0);
         }
         function _position(element) {
             if (element.parentElement == null)
